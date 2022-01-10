eeb992e23d39753fb4402af262ed6b3947214d0a
tonbei10 <47322051+tonbei10@users.noreply.github.com>
2022-01-10 22:14:55 +0900

    エンチャント本によるエンチャント付与の競合及びレベル制限の撤廃機能の追加

--- src/main/java/com/tonbei/scribe/Scribe.java
+++ src/main/java/com/tonbei/scribe/Scribe.java
@@ -12,8 +12,12 @@ import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.Repairable;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,6 +27,54 @@ public class Scribe extends JavaPlugin implements Listener {
     static final boolean debug = false;
 
     static Logger logger = null;
+
+    private static final int MAX_ENCHANT_LEVEL = 10;
+    private static final int MAX_ANVIL_USE_COUNT = 4;
+
+    private static final Map<Enchantment, Integer> ENCHANTMENT_COST;
+
+    static {
+        Map<Enchantment, Integer> temp = new HashMap<>();
+        temp.put(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
+        temp.put(Enchantment.PROTECTION_FIRE, 1);
+        temp.put(Enchantment.PROTECTION_FALL, 1);
+        temp.put(Enchantment.PROTECTION_EXPLOSIONS, 2);
+        temp.put(Enchantment.PROTECTION_PROJECTILE, 1);
+        temp.put(Enchantment.OXYGEN, 2);
+        temp.put(Enchantment.WATER_WORKER, 2);
+        temp.put(Enchantment.THORNS, 1);
+        temp.put(Enchantment.DEPTH_STRIDER, 2);
+        temp.put(Enchantment.FROST_WALKER, 2);
+        temp.put(Enchantment.BINDING_CURSE, 4);
+        temp.put(Enchantment.DAMAGE_ALL, 1);
+        temp.put(Enchantment.DAMAGE_UNDEAD, 1);
+        temp.put(Enchantment.DAMAGE_ARTHROPODS, 1);
+        temp.put(Enchantment.KNOCKBACK, 1);
+        temp.put(Enchantment.FIRE_ASPECT, 2);
+        temp.put(Enchantment.LOOT_BONUS_MOBS, 2);
+        temp.put(Enchantment.SWEEPING_EDGE, 2);
+        temp.put(Enchantment.DIG_SPEED, 1);
+        temp.put(Enchantment.SILK_TOUCH, 4);
+        temp.put(Enchantment.DURABILITY, 1);
+        temp.put(Enchantment.LOOT_BONUS_BLOCKS, 2);
+        temp.put(Enchantment.ARROW_DAMAGE, 1);
+        temp.put(Enchantment.ARROW_KNOCKBACK, 2);
+        temp.put(Enchantment.ARROW_FIRE, 2);
+        temp.put(Enchantment.ARROW_INFINITE, 4);
+        temp.put(Enchantment.LUCK, 2);
+        temp.put(Enchantment.LURE, 2);
+        temp.put(Enchantment.LOYALTY, 1);
+        temp.put(Enchantment.IMPALING, 2);
+        temp.put(Enchantment.RIPTIDE, 2);
+        temp.put(Enchantment.CHANNELING, 4);
+        temp.put(Enchantment.MULTISHOT, 2);
+        temp.put(Enchantment.QUICK_CHARGE, 1);
+        temp.put(Enchantment.PIERCING, 1);
+        temp.put(Enchantment.MENDING, 2);
+        temp.put(Enchantment.VANISHING_CURSE, 4);
+        temp.put(Enchantment.SOUL_SPEED, 4);
+        ENCHANTMENT_COST = Collections.unmodifiableMap(temp);
+    }
 
     @Override
     public void onEnable() {
@@ -73,6 +125,69 @@ public class Scribe extends JavaPlugin implements Listener {
                     ai.setRepairCost(ai.getRepairCost() + cost);
                     e.setResult(book);
                 }
+            }
+
+            EnchantmentStorageMeta secondMeta;
+
+            if (second.getType() == Material.ENCHANTED_BOOK && (secondMeta = (EnchantmentStorageMeta) second.getItemMeta()).hasStoredEnchants()) {
+                if (e.getResult() != null || result != null) {
+                    ai.setItem(2, null);
+                    e.setResult(null);
+                }
+
+                boolean isEnchantedBook = (first.getType() == Material.ENCHANTED_BOOK);
+                Map<Enchantment, Integer> enchantments = new HashMap<>();
+                (isEnchantedBook ? ((EnchantmentStorageMeta) first.getItemMeta()).getStoredEnchants() : first.getEnchantments()).forEach(
+                        (enchantment, level) -> enchantments.put(enchantment, (enchantment.getMaxLevel() <= 1) ? 1 : Math.min(MAX_ENCHANT_LEVEL, level)));
+
+                int cost = 0;
+                int repairCost = 0;
+
+                for (Map.Entry<Enchantment, Integer> ench : secondMeta.getStoredEnchants().entrySet()) {
+                    Enchantment enchant = ench.getKey();
+                    int addLevel = ench.getValue();
+                    boolean levelFlag = (enchant.getMaxLevel() <= 1);
+
+                    if (enchantments.containsKey(enchant)) {
+                        if (levelFlag) continue;
+
+                        int originalCost = enchantments.get(enchant);
+                        enchantments.put(enchant, addLevel = Math.min(MAX_ENCHANT_LEVEL, originalCost + addLevel));
+                        cost += ENCHANTMENT_COST.getOrDefault(enchant, 1) * Math.max(addLevel - originalCost, 0);
+                    } else {
+                        if (isEnchantedBook || enchant.canEnchantItem(first)) {
+                            enchantments.put(enchant, addLevel = (levelFlag ? 1 : Math.min(MAX_ENCHANT_LEVEL, addLevel)));
+                            cost += ENCHANTMENT_COST.getOrDefault(enchant, 1) * addLevel;
+                        }
+                    }
+                }
+
+                if (cost <= 0) return;
+
+                if (!isEnchantedBook) {
+                    if (first.getItemMeta() instanceof Repairable) {
+                        repairCost = ((Repairable) first.getItemMeta()).getRepairCost();
+                        cost += repairCost;
+                        repairCost = (int) Math.round(Math.pow(2, Math.min((int) Math.round((Math.log(repairCost + 1) / Math.log(2))) + 1, MAX_ANVIL_USE_COUNT))) - 1;
+                    }
+                }
+
+                ItemStack resultItem = first.clone();
+                if (isEnchantedBook) {
+                    EnchantmentStorageMeta bookMeta = (EnchantmentStorageMeta) resultItem.getItemMeta();
+                    enchantments.forEach((enchantment, level) -> bookMeta.addStoredEnchant(enchantment, level, true));
+                    resultItem.setItemMeta(bookMeta);
+                } else {
+                    if (resultItem.getItemMeta() instanceof Repairable) {
+                        ItemMeta repairableMeta = resultItem.getItemMeta();
+                        ((Repairable) repairableMeta).setRepairCost(repairCost);
+                        resultItem.setItemMeta(repairableMeta);
+                    }
+                    resultItem.addUnsafeEnchantments(enchantments);
+                }
+
+                e.setResult(resultItem);
+                e.getInventory().setRepairCost(cost);
             }
         }
     }
